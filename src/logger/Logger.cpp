e08bdd4005046e748d812acbda43f2af281261f6
Pavel Schon <pavel@schon.cz>
2016-06-20 22:22:19 +0200

    templateize logging

--- src/logger/Logger.cpp
+++ src/logger/Logger.cpp
@@ -15,17 +15,6 @@ namespace pyxchange
 namespace py = boost::python;
 
 
-namespace level
-{
-
-const std::string debug     = "debug";
-const std::string info      = "info";
-const std::string warning   = "warning";
-const std::string error     = "error";
-
-} /* namespace log */
-
-
 namespace attr
 {
 
@@ -61,50 +50,6 @@ py::object Logger::getLogger()
 
 
 /**
- * @brief Log message on debug level
- * @param message to be logged
- *
- */
-void Logger::debug( const boost::format& message )  const
-{
-    log( level::debug, message );
-}
-
-
-/**
- * @brief Log message on info level
- * @param message to be logged
- *
- */
-void Logger::info( const boost::format& message )  const
-{
-    log( level::info, message );
-}
-
-
-/**
- * @brief Log message on warning level
- * @param message to be logged
- *
- */
-void Logger::warning( const boost::format& message )  const
-{
-    log( level::warning, message );
-}
-
-
-/**
- * @brief Log message on error level
- * @param message to be logged
- *
- */
-void Logger::error( const boost::format& message )  const
-{
-    log( level::error, message );
-}
-
-
-/**
  * @brief Log message on any level
  * @param level as string
  * @param message to be logged
@@ -112,13 +57,8 @@ void Logger::error( const boost::format& message )  const
  */
 void Logger::log( const std::string& level, const boost::format& message ) const
 {
-    if( logger != py::object() ) // if logger is not None
-    {
-        logger.attr( level.c_str() )( message.str() );
-    }
+    logger.attr( level.c_str() )( message.str() );
 }
-
-
 
 
 } /* namespace pyxchange */
--- src/logger/Logger.hpp
+++ src/logger/Logger.hpp
@@ -19,10 +19,17 @@ class Logger
 public:
                 Logger();
 
-    void        debug( const boost::format& message ) const;
-    void        info( const boost::format& message )  const;
-    void        warning( const boost::format& message )  const;
-    void        error( const boost::format& message )  const;
+    template<typename... Params>
+    void        debug( const boost::format& message, Params... params ) const;
+
+    template<typename... Params>
+    void        info( const boost::format& message, Params... params )  const;
+
+    template<typename... Params>
+    void        warning( const boost::format& message, Params... params )  const;
+
+    template<typename... Params>
+    void        error( const boost::format& message, Params... params )  const;
 
     static const std::string name;
 
@@ -31,8 +38,86 @@ private:
 
     void        log( const std::string& level, const boost::format& message ) const;
 
+    template<typename Param, typename... Params>
+    void        log( const std::string& level, const boost::format& message,
+                      const Param& param, Params... params ) const;
+
     const boost::python::object logger;
 };
+
+
+namespace level
+{
+
+const std::string debug     = "debug";
+const std::string info      = "info";
+const std::string warning   = "warning";
+const std::string error     = "error";
+
+} /* namespace log */
+
+
+/**
+ * @brief Log message on debug level
+ * @param message to be logged
+ *
+ */
+template<typename... Params>
+void Logger::debug( const boost::format& message, Params... params )  const
+{
+    log( level::debug, message, params... );
+}
+
+
+/**
+ * @brief Log message on info level
+ * @param message to be logged
+ *
+ */
+template<typename... Params>
+void Logger::info( const boost::format& message, Params... params )  const
+{
+    log( level::info, message, params... );
+}
+
+
+/**
+ * @brief Log message on warning level
+ * @param message to be logged
+ *
+ */
+template<typename... Params>
+void Logger::warning( const boost::format& message, Params... params )  const
+{
+    log( level::warning, message, params... );
+}
+
+
+/**
+ * @brief Log message on error level
+ * @param message to be logged
+ *
+ */
+template<typename... Params>
+void Logger::error( const boost::format& message, Params... params )  const
+{
+    log( level::error, message, params... );
+}
+
+
+/**
+ * @brief Log message on any level
+ * @param level as string
+ * @param message to be logged
+ * @param param formatting parameter
+ *
+ */
+template<typename Param, typename... Params>
+inline void Logger::log( const std::string& level, const boost::format& message,
+                         const Param& param, Params... params ) const
+{
+    log( level, boost::format( message ) % param, params... );
+}
 
 
 } /* namespace pyxchange */
